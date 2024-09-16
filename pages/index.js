b50090a3c3502b651a4ee970d56dd9e92efbf0ce
Akinori35 <120032638+Akinori35@users.noreply.github.com>
2024-09-16 15:02:21 +0000

    mudanca nas cores

--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,15 @@ import Titulo from "./titulo";
 function index(){
     return <div>
         <Titulo titulo="Jump pedrinho jump!!"/>
-        <CapsLock name="caio" idade="10"/>
-        <CapsLock name="jao" idade="11"/>
-        <CapsLock name="pedro" idade="12"/>
+        <CapsLock name="caio" idade="10" nota="10" cor="blue"/>
+        <CapsLock name="jao" idade="11" nota="10" cor="orange"/>
+        <CapsLock name="pedro" idade="12" nota="10" cor="yellow"/>
         </div>
 
 }
-function CapsLock({name, idade}) {
+function CapsLock({name, idade, cor}) {
     //console.log(param);
     //return <div>(name)
-    return <div>{name} {idade?idade:"sem idade"}</div> 
+    return <div style={{color:cor}}>{name} {idade?idade:"sem idade"}</div> 
 }
 export default index;
